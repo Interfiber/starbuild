3f177edc5b6c3d6676a003af25de4139768337a0
Interfiber <hunterstasonis@icloud.com>
2021-05-06 08:42:20 -0400

    Update starbuild.sh

--- starbuild.sh
+++ starbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Set luapath
-export LUA_PATH=$STARBUILD/lib/\?.lua\;$STARBUILD/plugins/\?.lua\;
+export LUA_PATH=$STARBUILD/lib/\?.lua\;$STARBUILD/plugins/\?.lua\;$PWD/\?.lua\;
 lua $STARBUILD/lib/starbuild_main.lua $@
