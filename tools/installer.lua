28271c58ee35286e4b3682b0a2cf79e55fe507c6
Interfiber <hunterstasonis@icloud.com>
2021-05-06 08:48:34 -0400

    :package: :sparkles: Update installer tools

--- tools/installer.lua
+++ tools/installer.lua
@@ -1,4 +1,10 @@
+print("Running bootstrap...")
 local Git = "https://github.com/Interfiber/starbuild.git"
 os.execute("git clone "..Git.." .star")
 os.execute("cd .star && ./configure --bootstrap")
 os.execute("rm -rf .star")
+print("Great! starbuild is now installed, simply paste the following")
+print("lines into your shell profile")
+print("export STARBUILD=$HOME/.local/share/starbuild")
+print("export PATH=$PATH:$STARBUILD/bin")
+print("Then reload your shell profile, or reopen your terminal.")
