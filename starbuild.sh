0d597892d4f8d5f9a8eb39d6837ff39ba315b7cf
Interfiber <interfiber@protonmail.com>
2021-05-11 09:48:44 -0400

    :bug: Fix small bugs

--- starbuild.sh
+++ starbuild.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 # Set luapath
+if [ -z "$STARBUILD" ]; then
+  echo "Error: STARBUILD is not set! Please set this varible and continue"
+  exit 1
+fi
 export LUA_PATH=$STARBUILD/lib/\?.lua\;$STARBUILD/plugins/\?.lua\;$PWD/\?.lua\;
 lua $STARBUILD/lib/starbuild_main.lua $@
